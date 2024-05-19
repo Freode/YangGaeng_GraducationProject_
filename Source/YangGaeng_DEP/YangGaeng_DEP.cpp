7183e923b3a14545cdda2776e9217d609c8cc0ed
Yeonkyeong KIM <kimi070464@gmail.com>
2024-05-19 11:06:23 +0900

    c++ 변경

---
+++ Source/YangGaeng_DEP/YangGaeng_DEP.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "YangGaeng_DEP.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YangGaeng_DEP, "YangGaeng_DEP" );
