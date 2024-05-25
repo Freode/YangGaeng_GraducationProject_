8cd6700167b0c5f8972e6685a528a0c0201f8480
Freode <free4760@naver.com>
2024-05-25 23:59:48 +0900

    Change C++ Classes 11

--- Source/YangGaeng_DEP/Private/GasValve.cpp
+++ Source/YangGaeng_DEP/Private/GasValve.cpp
@@ -3,3 +3,60 @@
 
 #include "GasValve.h"
 
+AGasValve::AGasValve()
+{
+	ValveRockTimeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("VALVEROCKTIMELINE"));
+
+	static ConstructorHelpers::FObjectFinder<UCurveFloat> CURVE(TEXT("/Game/YangGaeng/Data/Curve_LinearSecond.Curve_LinearSecond"));
+	if (CURVE.Succeeded())
+	{
+		MyCurve = CURVE.Object;
+	}
+}
+
+void AGasValve::InteractEvent_Implementation(APlayerLevelCharacter* Character, bool bIsUI_Expression)
+{
+	Super::InteractEvent_Implementation(Character, bIsUI_Expression);
+
+	if (bCanOperate)
+	{
+		bCanOperate = false;
+		ValveRockTimeline->PlayFromStart();
+	}
+
+}
+
+void AGasValve::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	ValveRockTimeline->TickComponent(DeltaTime, ELevelTick::LEVELTICK_TimeOnly, nullptr);
+}
+
+void AGasValve::HandleProgress(float value)
+{
+	SetActorRotation(FRotator(0.0f, 90.0f, value * 90.0f));
+}
+
+void AGasValve::HandleTimelineFinished()
+{
+	EndEvent();
+}
+
+void AGasValve::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (MyCurve)
+	{
+		FOnTimelineFloat ProgressFunction;
+		ProgressFunction.BindUFunction(this, FName("HandleProgress"));
+		ValveRockTimeline->AddInterpFloat(MyCurve, ProgressFunction);
+
+		FOnTimelineEvent TimelineFinishedFunction;
+		TimelineFinishedFunction.BindUFunction(this, FName("HandleTimelineFinished"));;
+		ValveRockTimeline->SetTimelineFinishedFunc(TimelineFinishedFunction);
+
+		ValveRockTimeline->SetLooping(false);
+	}
+}
